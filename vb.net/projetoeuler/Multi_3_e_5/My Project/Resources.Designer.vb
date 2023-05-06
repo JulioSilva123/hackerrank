6402904b0a9965d3351e0c6cad67569b34d0cf0e
Julio <86787811+JulioSilva123@users.noreply.github.com>
2023-05-05 23:32:43 -0300

    CSharp Projeto Euler Multi 3 e 5

--- vb.net/projetoeuler/Multi_3_e_5/My Project/Resources.Designer.vb
+++ vb.net/projetoeuler/Multi_3_e_5/My Project/Resources.Designer.vb
@@ -39,7 +39,7 @@ Namespace My.Resources
         Friend ReadOnly Property ResourceManager() As Global.System.Resources.ResourceManager
             Get
                 If Object.ReferenceEquals(resourceMan, Nothing) Then
-                    Dim temp As Global.System.Resources.ResourceManager = New Global.System.Resources.ResourceManager("Multi_3_e_5VB.Resources", GetType(Resources).Assembly)
+                    Dim temp As Global.System.Resources.ResourceManager = New Global.System.Resources.ResourceManager("Multi_3_e_5_VB.Resources", GetType(Resources).Assembly)
                     resourceMan = temp
                 End If
                 Return resourceMan
--- vb.net/projetoeuler/Multi_3_e_5/My Project/Settings.Designer.vb
+++ vb.net/projetoeuler/Multi_3_e_5/My Project/Settings.Designer.vb
@@ -64,9 +64,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.Multi_3_e_5VB.My.MySettings
+        Friend ReadOnly Property Settings() As Global.Multi_3_e_5_VB.My.MySettings
             Get
-                Return Global.Multi_3_e_5VB.My.MySettings.Default
+                Return Global.Multi_3_e_5_VB.My.MySettings.Default
             End Get
         End Property
     End Module
